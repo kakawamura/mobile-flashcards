7c401e89e87fc1c720c0ea92decd6f403b6df8c6
Kazushi Kawamura <wakaka2277@gmail.com>
2018-01-08 11:40:31 +0900

    feat: Add load font

--- App.js
+++ App.js
@@ -34,9 +34,23 @@ const Stack = StackNavigator({
 });
 
 export default class App extends React.Component {
+  componentWillMount() {
+    this.loadFonts();
+  }
+
+  async loadFonts() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({ isReady: true });
+  }
+
   componentDidMount() {
     setLocalNotification();
   }
+
   render() {
     return (
       <View style={styles.container}>
